ab941a837b23b49ef917a80bc031d66ebb04affe
Khanis Nikolay <hn@wnstudio.ru>
2017-11-29 15:49:42 +0300

    Init

---
+++ js/feedback-s.php
@@ -0,0 +1,16 @@
+<?php
+$name = $_POST["name"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+$to = "hn@wnstudio.ru";
+$subject = "Сотрудничество";
+$text =  "Написал(а): $name\n Контактный телефон - $phone\n\n email: $email\n";
+$header.= "Content-type: text/html; charset=utf-8\r\n";
+$header .= "MIME-Version: 1.0\r\n";
+$mail = mail($to, $subject, $text, $headers);
+if($mail){
+   echo "success";
+} else {
+        echo "errorMSG";
+    }
+?>
